507a7b55e168a313992827dc226c60710c16dbe7
Gary Chiu <q2ddcmtjcp@privaterelay.appleid.com>
2025-12-28 00:01:09 +0800

    Update test_protocol.py

--- core/tests/test_protocol.py
+++ core/tests/test_protocol.py
@@ -7,7 +7,7 @@ from bmarker.core.src.protocol import *
 def test_core():
     assert time_to_samples(1, 's') == 44100
     assert time_to_samples(500, 'ms') == 22050
-    assert convert_markers([48000], 48000, 44100)[0] == 44099
+    assert abs(convert_markers([48000], 48000, 44100)[0] - 44099) <= 1
     pulses = generate_simple_pulses([100, 200], 500)
     stats = validate_jitter(pulses, 100)
     assert stats['pulse_count'] == 2
