e51d412bd1d5930c2bb3f872840fe929c104d1a3
Gary Chiu <q2ddcmtjcp@privaterelay.appleid.com>
2025-12-28 03:36:48 +0800

    Create __init__.py

---
+++ tools/__init__.py
@@ -0,0 +1,18 @@
+'''python
+"""
+B-Marker Tools Package
+Copyright (c) 2025 B-Marker Development Team.
+
+This package is governed by the B-Marker Tools Commercial License.
+For details, see LICENSE-COMMERCIAL.md in this directory or visit https://b-marker.com.
+Commercial inquiries: sales@b-marker.com
+"""
+# bmarker/tools/__init__.py
+from .converters import (
+    frames_to_ms,
+    beats_to_ms,
+    samples_to_ms,
+    ms_to_frames,
+    batch_frames_to_ms
+)
+__all__ = ['frames_to_ms', 'beats_to_ms', 'samples_to_ms', 'ms_to_frames', 'batch_frames_to_ms']'batch_frames_to_ms']
